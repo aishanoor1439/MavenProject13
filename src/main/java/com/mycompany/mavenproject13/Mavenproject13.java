3a4176b97fad6d0a2da5e98dcb06b6924850fb3a
aishanoor1439 <aisha.noor1439@gmail.com>
2025-02-23 23:10:10 +0100

    First Commit
    added project files

---
+++ src/main/java/com/mycompany/mavenproject13/Mavenproject13.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject13;
+
+/**
+ *
+ * @author ABC
+ */
+public class Mavenproject13 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
